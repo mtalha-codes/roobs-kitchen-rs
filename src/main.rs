d10590643a9444e407b1425188e2520878af668f
Muhammad Talha <khankkhan123apsacs@gmail.com>
2025-08-26 13:45:28 +0500

    no big changes
    no desc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod types;
+use types::email::Email;
 fn main() {
-    println!("I am an Improved Rust Rewrite of `Roob's Kitchen App` which was initially written in C++\nYou can say it is an improved version of the Old C++ Edition\nCurrently It is Unstable and Underdeveloped\n!!! Work In Progress !!!\n");
+
+
 }
